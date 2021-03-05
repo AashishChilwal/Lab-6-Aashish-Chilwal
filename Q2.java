535960c4f9256a467e5032ce3bdc2b1e5bc04f16
AashishChilwal <78070029+AashishChilwal@users.noreply.github.com>
2021-03-05 16:41:47 -0700

    Finished Q2

--- Q2.java
+++ Q2.java
@@ -1,1 +1,32 @@
+import java.util.Scanner;
+public class Q2
+{
+	public static void main(String[] args) {
+		 int coinflip = (int) (Math.random() * 2.0);
+		 char coin;
+		 Scanner guess = new Scanner(System.in);
+		 System.out.print("Guess head or tail (H or T)? ");
+		 char s = guess.next().charAt(0); ;
+		 if (coinflip == 0)
+		 {
+		    coin = 'H';
+		 } else
+		 {
+		     coin = 'T';
+		 }
+		 if (s == 'H' && coin == 'H')
+		 {
+		    System.out.println("Correct! You can read my mind.");
+		 } else if (s == 'T' && coin == 'T')
+		 {
+		     System.out.println("Correct! You can read my mind.");
+		 } else if (s == 'H' && coin == 'T')
+		 {
+		      System.out.println("Sorry, it was tails");
+		 }else
+		 {
+		     System.out.println("Sorry, it was heads");
+		 }
+		 }
+		 }
 
